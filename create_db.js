f7423f850c9a631abe0671dc5f421bf6ab28881e
Guilherme Lopes <guilherme.j.lopes@ufv.br>
2025-12-11 12:56:02 -0300

    db

--- create_db.js
+++ create_db.js
@@ -9,7 +9,7 @@ const USERNAME = 'guilherme';
 const baseConfig = {
     host: process.env.PGHOST || 'localhost',
     user: process.env.PGUSER || 'postgres',
-    password: process.env.PGPASSWORD || '',
+    password: 'Ash&314ka2' || '',
     port: process.env.PGPORT ? Number(process.env.PGPORT) : 5432,
     // database will be supplied per-connection
 };
